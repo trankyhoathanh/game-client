34ee171909124fd76153f6052048de671510c104
Thanh TKH <>
2021-07-03 03:12:55 +0700

    Update async await for client

--- client_2.js
+++ client_2.js
@@ -1,9 +1,11 @@
 const io = require('socket.io-client');
 const lib_math = require('./lib/lib_math')
-let ahead = 'T'
-let user_from = 1
-let user_to = 500
-let sleep_result = 50
+let ahead = 'S'
+let user_from = 501
+let user_to = 1000
+let sleep_result = 0
+let sleep_connect_before = 10
+let sleep_connect_after = 10
 let total_done = 0
 let unlimited = false
 // let socket_url = 'http://game.tranthanh92.com'
@@ -17,19 +19,23 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-}
+// function sleep(milliseconds) {
+//     const date = Date.now();
+//     let currentDate = null;
+//     do {
+//       currentDate = Date.now();
+//     } while (currentDate - date < milliseconds);
+// }
 
-function task () {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+(async () => {
     try {
         for (let i = user_from; i < user_to; i++ ) {
             let item_socket = io(socket_url);
-            item_socket.on('login_user_id_return', (output) => {
+            item_socket.on('login_user_id_return', async (output) => {
                 console.log(output)
                 if (output.user) {
                     if (output.user.room_trans) {
@@ -42,30 +48,15 @@ function task () {
                         }
                         console.log(`login_user_id_return ${item_send.user_id} ${item_send.room_trans} ${item_send.choose}`)
                        
-                        sleep(sleep_result)
+                        await sleep(sleep_result)
                         item_socket.emit('send_choose', item_send);
-
-                        // // Step này check null thì không làm gì nữa
-                        // if (output.user.room_id && output.user.room_trans) {
-                        //     let item_send = {
-                        //         room_id: output.user.room_id,
-                        //         room_trans: output.user.room_trans,
-                        //         user_id: output.user.user_id,
-                        //         choose: getRandomInt(1,3)
-                        //     }
-
-                        //     console.log(`login_user_id_return ${item_send.user_id} ${item_send.room_trans} ${item_send.choose}`)
-
-                        //     sleep(sleep_result)
-                        //     item_socket.emit('send_choose', item_send);
-                        // }
                     } else {
                         // Login lại chơi ván mới
                         // item.socket.emit('login_user_id', { user_id : item.user_id });
                     }
                 }
             })
-            item_socket.on('send_choose_return', (output) => {
+            item_socket.on('send_choose_return', async (output) => {
                 if (output.status && output.room_trans) {
 
                     // Step này check null thì không làm gì nữa
@@ -77,7 +68,7 @@ function task () {
                             choose: getRandomInt(1,3)
                         }
                         console.log(`send_choose_return ${item_send.user_id} ${item_send.room_trans} ${item_send.choose}`)
-                        sleep(sleep_result)
+                        await sleep(sleep_result)
                         item_socket.emit('send_choose', item_send);
                     } else {
                         // Login lại chơi ván mới
@@ -93,23 +84,26 @@ function task () {
                     }
                 }
             })
-
             sockets.push({
                 socket: item_socket,
                 user_id: `${ahead}${lib_math.zeroFill(i,4)}`
             })
-        }
-        
-        for(let item of sockets) {
-            sleep(sleep_result);
-            item.socket.connect();
-            item.socket.emit('login_user_id', { user_id : item.user_id });
-        }
-        
-       
-    } catch (err) {
-      console.log(err)
-    }
-}
 
-task()
+            await sleep(sleep_connect_before);
+            item_socket.connect();
+            await sleep(sleep_connect_after);
+            console.log(`WS connect user ${`${ahead}${lib_math.zeroFill(i,4)}`}`);
+            item_socket.emit('login_user_id', { user_id : `${ahead}${lib_math.zeroFill(i,4)}` });
+        }
+        
+        // for(let item of sockets) {
+        //     await sleep(sleep_connect_before);
+        //     item.socket.connect();
+        //     await sleep(sleep_connect_after);
+        //     console.log(`WS connect user ${item.user_id}`);
+        //     item.socket.emit('login_user_id', { user_id : item.user_id });
+        // }
+    } catch (e) {
+        console.log(e)
+    }
+})();
